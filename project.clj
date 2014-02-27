81ba4f809665fff51d3ce5848cb0a170a98b8836
ken restivo <ken@restivo.org>
2014-02-27 11:33:06 -0800

    Update to latest versions of a few things, and also make it in line with recommendations from om docs.

--- project.clj
+++ project.clj
@@ -3,24 +3,28 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [om "0.2.3"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
-    :builds [{:id "binclock"
-              :source-paths ["src"]
-              :compiler {:output-to "binclock.js"
-                         :output-dir "out"
-                         :optimizations :none
-                         :source-map true}}
-             {:id "release"
-              :source-paths ["src"]
-              :compiler {:output-to "binclock-min.js"
-                         :optimizations :advanced
-                         :externs ["react.js"]
-                         :output-wrapper true
-                         :pretty-print false}}]})
+  :cljsbuild {:builds [{:id "dev" 
+                        :source-paths ["src"]
+                        :compiler {:output-to "binclock.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map true}}
+
+                       
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "binclock-min.js"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :preamble ["react/react.min.js"]
+                                   ;; :output-wrapper true ;; don't know why this would be necessary?
+                                   :externs ["react/externs/react.js"]}}]})
+
